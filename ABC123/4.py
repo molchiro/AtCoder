4de1ca1ecbd25f457c3ea61aa6204139f5ceaaac
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-04-09 23:51:20 +0900

    D TLE

--- ABC123/4.py
+++ ABC123/4.py
@@ -3,58 +3,25 @@ A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 C = list(map(int, input().split()))
 
-A.sort()
-B.sort()
-C.sort()
+A.sort(reverse=True)
+B.sort(reverse=True)
+C.sort(reverse=True)
 
-Ai = X-1
-Bi = Y-1
-Ci = Z-1
+AB = []
+for i in range(min(X,K)):
+    for j in range(min(Y,K)):
+        AB.append(A[i] + B[j])
 
-candidates = [[A[Ai], B[Bi], C[Ci]],
-              [A[Ai-1], B[Bi], C[Ci]],
-              [A[Ai], B[Bi-1], C[Ci]],
-              [A[Ai], B[Bi], C[Ci-1]],
-              [A[Ai-1], B[Bi-1], C[Ci]],
-              [A[Ai], B[Bi-1], C[Ci-1]],
-              [A[Ai-1], B[Bi], C[Ci-1]],
-              [A[Ai-1], B[Bi-1], C[Ci-1]]
-              ]
-trigger_A = [A[Ai-1], B[Bi], C[Ci]]
-trigger_B = [A[Ai], B[Bi-1], C[Ci]]
-trigger_C = [A[Ai], B[Bi], C[Ci-1]]
+AB.sort(reverse=True)
 
+ABC = []
+for i in range(min(X*Y,K)):
+    for j in range(min(Z,K)):
+        ABC.append(AB[i] + C[j])
+
+ABC.sort(reverse=True)
 
 for i in range(K):
-    max_set = max(candidates, key=(lambda x: sum(x)))
-    candidates.remove(max_set)
-    print(candidates)
-    print(sum(max_set))
-    if Ai > 1 and not (trigger_A in candidates):
-        Ai -= 1
-        candidates.extend([
-            [A[Ai-1], B[Bi], C[Ci]],
-            [A[Ai-1], B[Bi-1], C[Ci]],
-            [A[Ai-1], B[Bi], C[Ci-1]],
-            [A[Ai-1], B[Bi-1], C[Ci-1]]
-        ])
-        trigger_A = min(filter((lambda x: x[0] == A[Ai]), candidates), key=(lambda x: sum(x)))
-    if Bi > 1 and not (trigger_B in candidates):
-        Bi -= 1
-        candidates.extend([
-            [A[Ai], B[Bi-1], C[Ci]],
-            [A[Ai-1], B[Bi-1], C[Ci]],
-            [A[Ai], B[Bi-1], C[Ci-1]],
-            [A[Ai-1], B[Bi-1], C[Ci-1]]
-        ])
-        trigger_B = min(filter((lambda x: x[1] == B[Bi]), candidates), key=(lambda x: sum(x)))
-    if Ci > 1 and not (trigger_C in candidates):
-        Ci -= 1
-        candidates.extend([
-            [A[Ai], B[Bi], C[Ci-1]],
-            [A[Ai], B[Bi-1], C[Ci-1]],
-            [A[Ai-1], B[Bi], C[Ci-1]],
-            [A[Ai-1], B[Bi-1], C[Ci-1]]
-        ])
-        trigger_C = min(filter((lambda x: x[2] == C[Ci]), candidates), key=(lambda x: sum(x)))
+    print(ABC[i])
+
 

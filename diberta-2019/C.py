4ea1f9a7549d69509e4a6c7b84813166c625e1ce
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-06-15 16:21:42 +0900

    C AC

--- diberta-2019/C.py
+++ diberta-2019/C.py
@@ -1,16 +1,26 @@
 N = int(input())
 
-AB_cnt = 0
-A_cnt = 0
-B_cnt = 0
+innerAB_cnt = 0
+endA_cnt = 0
+startB_cnt = 0
+B_A_cnt = 0
 for i in range(N):
     S = input()
-    for j in range(len(S)-1):
-        if S[j:j+2] == 'AB':
-            AB_cnt += 1
-    if S[-1] == 'A':
-        A_cnt += 1
-    if S[0] == 'B':
-        B_cnt += 1
+    innerAB_cnt += S.count('AB')
+    is_end_A = S[-1] == 'A'
+    is_start_B = S[0] == 'B'
+    if is_end_A and is_start_B:
+        B_A_cnt += 1
+    elif is_end_A:
+        endA_cnt += 1
+    elif is_start_B:
+        startB_cnt += 1
+remainder = abs(startB_cnt - endA_cnt)
+if B_A_cnt == 0:
+    print(min(endA_cnt, startB_cnt) + innerAB_cnt)
+elif endA_cnt + startB_cnt > 0:
+    print(min(endA_cnt, startB_cnt) + B_A_cnt + innerAB_cnt)
+else:
+    print(B_A_cnt-1 + innerAB_cnt)
 
-print(AB_cnt + min(A_cnt, B_cnt))
+

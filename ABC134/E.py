dbca5ca72ce65ac57a504eb43825bc69fb7602b9
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2019-07-21 16:53:49 +0900

    E use "bisect_left" => "bisect"

--- ABC134/E.py
+++ ABC134/E.py
@@ -5,7 +5,7 @@ A = [-int(input()) for i in range(N)]
 X = []
 for i in range(N):
     a = A[i]
-    idx = bisect.bisect_left(X, a+0.1)
+    idx = bisect.bisect(X, a)
     if idx == len(X):
         X.append(a)
     else:

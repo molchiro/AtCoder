c32eb3ce4de1eb4cecde8c90bcc1ec75f3fd0bcd
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2020-04-04 20:55:43 +0900

    D AC after contest

--- ABC160/D.py
+++ ABC160/D.py
@@ -1,28 +1,10 @@
+import collections
+
 N, X, Y = list(map(int, input().split()))
-neighbor = { i: [] for i in range(1, N+1) }
-for i in range(1,N):
-    neighbor[i].append(i+1)
-    neighbor[N-i+1].append(N-i)
-neighbor[X].append(Y)
-neighbor[Y].append(X)
-
-distanced_neighbor = { i: { j: [] for j in range(1, N)} for i in range(1, N+1) }
-for i in range(1, N+1):
-    visited = [i]
-    next_nodes = neighbor[i]
-    for j in range(1, N):
-        distanced_neighbor[i][j] = next_nodes[:]
-        queue = next_nodes[:]
-        visited += next_nodes
-        next_nodes = []
-        for q in queue:
-            for node in neighbor[q]:
-                if node in visited:
-                    continue
-                else:
-                    next_nodes.append(node)
-        next_nodes = list(set(next_nodes))
-
-for i in range(1, N):
-    print(sum([len(distanced_neighbor[j][i]) for j in range(1, N+1)])//2)
-    
+dists = [
+        min(abs(j-i), abs(X-1-i)+1+abs(Y-1-j), abs(Y-1-i)+1+abs(X-1-j))
+        for i in range(N-1) for j in range(i+1, N)
+    ]
+c = collections.Counter(dists)
+ans = [c[K] for K in range(1,N)]
+print(*ans, sep='\n')

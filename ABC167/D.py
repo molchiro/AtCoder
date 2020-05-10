020a68697af1bc5e1d36c63d8f08637048dfbe42
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2020-05-11 02:36:53 +0900

    refactor

--- ABC167/D.py
+++ ABC167/D.py
@@ -1,22 +1,21 @@
 N, K = list(map(int, input().split()))
-K -= 1
 A = list(map(lambda x: int(x) - 1, input().split()))
-seen = [None]*N
-seen_set = set()
+
 now = 0
-for i in range(N):
-    seen[i] = now
-    seen_set.add(now)
-    to = A[now]
-    now = to
-    if now in seen_set:
-        seen[i+1] = now
-        loop_s = seen.index(now)
-        loop_end = i + 1
-        size = loop_end - loop_s
+seen = []
+while True:
+    seen.append(now)
+    teleporter = A[now]
+    A[now] = 'used'
+    if teleporter == 'used':
         break
+    else:
+        now = teleporter  
+
+loop_s = seen.index(now)
+size = len(seen) - loop_s - 1
+
 if K <= loop_s:
-    print(seen[K+1] + 1)
+    print(seen[K] + 1)
 else:
-    K -= loop_s
-    print(seen[loop_s + 1 + K % size] + 1)
+    print(seen[loop_s +(K - loop_s) % size] + 1)

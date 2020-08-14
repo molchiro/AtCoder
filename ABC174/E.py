0c59f910b525043f9f5bd74a6e9cc03de3f43597
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2020-08-15 02:26:52 +0900

    simplify

--- ABC174/E.py
+++ ABC174/E.py
@@ -1,22 +1,13 @@
-from math import ceil
-
-def check(A, K, L):
-    i = 0
-    for a in A:
-        i += ceil(a/L)-1
-        if i > K:
-            return False
-    return True
-
 N, K = list(map(int, input().split()))
 A = list(map(int, input().split()))
-A.sort(reverse=True)
+
 l = 0
-r = A[0]
+r = max(A)
 while l + 1 < r:
     m = (l+r)//2
-    if check(A, K, m):
-        r = m
-    else:
+    n = sum([(x-1)//m for x in A])
+    if n > K:
         l = m
+    else:
+        r = m
 print(r)

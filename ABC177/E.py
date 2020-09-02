311418879742b2636b474d023316a60e4c43b7a8
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2020-09-02 23:29:50 +0900

    E fast and symple

--- ABC177/E.py
+++ ABC177/E.py
@@ -1,36 +1,27 @@
-# 入力
 N = int(input())
-A = list(map(int, input().split()))
+A = [0]*(1000001)
+for a in list(map(int, input().split())):
+    A[a] += 1
 
-R = max(A)
-
-# D[x]にxを割り切れる最初の素数を格納
-# 次に行う素因数分解で試し割りのムダを削減するための前準備
-D = [0]*(R+1)
-for i in range(2, R+1):
-    if D[i]:
+# MFPF = Most Frequent Prime Factor
+MFPF_n = 0
+# 範囲内のすべての素数について、割り切れるようなAiがいくつ存在するか数える
+# エラトステネスの篩で計算量削減
+seen = [0]*(1000001)
+for i in range(2, 1000001):
+    if seen[i]:
         continue
     n = i
-    while n < R+1:
-        if D[n] == 0:
-            D[n] = i
+    ct = 0
+    while n < 1000001:
+        ct += A[n]
+        seen[n] = 1
         n += i
+    MFPF_n = max(MFPF_n, ct)
 
-# 素因数分解し、素因子をカウント
-# ex: 12 => 2と3のカウントを+1する
-prime_factor_counter = [0]*(R+1)
-for a in A:
-    tmp = a
-    while tmp > 1:
-        prime_factor = D[tmp]
-        prime_factor_counter[prime_factor] += 1
-        while tmp%prime_factor == 0:
-            tmp //= prime_factor
-
-# 回答出力
-if max(prime_factor_counter) < 2:
+if MFPF_n < 2:
     print('pairwise coprime')
-elif max(prime_factor_counter) - A.count(1) < N:
+elif MFPF_n - A[1] < N:
     print('setwise coprime')
 else:
     print('not coprime')

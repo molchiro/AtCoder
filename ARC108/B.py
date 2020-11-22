17d6dbaf0ee093b91128ee8ba4cfb3a2c566a699
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2020-11-22 18:42:34 +0900

    refactor

--- ARC108/B.py
+++ ARC108/B.py
@@ -1,21 +1,13 @@
-from collections import deque
-
 N = int(input())
 s = input()
-f = 0
-fo = 0
 
-if N < 3:
-    print(N)
-else:
-    ans = N
-    s = 'aa' + s
-    d = deque([3, 2, 1, 0])
-    for i in range(4, N+2):
-        if s[i] == 'x' and s[d[0]] == 'o' and s[d[1]] == 'f':
-            ans -= 3
-            d.popleft()
-            d.popleft()
-        else:
-            d.insert(0, i)
-    print(ans)
+ans = N
+s_ = ['*', '*']
+for i in range(N):
+    s_ += s[i]
+    if s_[-3:] == ['f', 'o', 'x']:
+        ans -= 3
+        s_.pop()
+        s_.pop()
+        s_.pop()
+print(ans)

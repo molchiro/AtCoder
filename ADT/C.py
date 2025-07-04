18bdeb2ec0b4da4726425948715779d07c559c7f
Hiroyuki Ikuta <i.hiro.0513@gmail.com>
2025-07-04 19:22:02 +0900

    ADTテンプレ

--- ADT/C.py
+++
@@ -1,5 +0,0 @@
-N, Q = list(map(int, input().split()))
-hoge = [list(map(int, input().split())) for _ in range(N)]
-for _ in range(Q):
-    s, t = list(map(int, input().split()))
-    print(hoge[s-1][t])
--- ADT/G.py
+++
@@ -1,5 +0,0 @@
-T = int(input())
-for _ in range(T):
-    a, s = list(map(int, input().split()))
-    s -= 2*a
-    print('Yes' if s >= 0 else 'No')
